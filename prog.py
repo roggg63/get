403f41907a8dcc9b5e58e6ac6c28389378270f11
roggg63 <nik.rogov.07@mail.ru>
2026-04-09 18:45:41 +0300

    python

---
+++ prog.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+number = 26
+state = 0
+period = 1.0
+
+GPIO.setup(number, GPIO.OUT)
+
+while True:
+    GPIO.output(number, state)
+    state = not state
+    time.sleep(period)
