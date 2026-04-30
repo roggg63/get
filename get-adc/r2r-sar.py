ae4414bec8cb633944e8b9daf0a84af418261ccb
roggg63 <nik.rogov.07@mail.ru>
2026-04-30 19:15:20 +0300

    Create r2r-sar.py

---
+++ get-adc/r2r-sar.py
@@ -0,0 +1,42 @@
+import time
+from r2r_adc import R2R_ADC
+from adc_plot import plot_voltage_vs_time, plot_sampling_period_hist
+
+def main():
+    DYNAMIC_RANGE = 3.30
+    DURATION = 3.0
+    COMPARE_TIME = 0.001
+
+    voltage_values = []
+    time_values = []
+
+    try:
+        adc = R2R_ADC(dynamic_range=DYNAMIC_RANGE,
+                      compare_time=COMPARE_TIME,
+                      verbose=False)
+
+        start_time = time.time()
+        print(f"Измеряем напряжение методом SAR в течение {DURATION} секунд...")
+
+        while time.time() - start_time < DURATION:
+            v = adc.get_sar_voltage()
+            t = time.time() - start_time
+            voltage_values.append(v)
+            time_values.append(t)
+
+        print("Измерения завершены.")
+        print("Строим график напряжения от времени...")
+        plot_voltage_vs_time(time_values, voltage_values, DYNAMIC_RANGE)
+
+        print("Строим гистограмму длительности измерений...")
+        plot_sampling_period_hist(time_values)
+
+    except KeyboardInterrupt:
+        print("\nПрерывание пользователем")
+
+    finally:
+        if 'adc' in locals():
+            adc.cleanup()
+
+if __name__ == "__main__":
+    main()
