a6ac2511053cd14c6e0c4d2275b81adb139671ec
roggg63 <nik.rogov.07@mail.ru>
2026-04-30 18:24:03 +0300

    Update r2r_adc.py

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -1,16 +1,17 @@
-
 import RPi.GPIO as GPIO
 import time
 
 class R2R_ADC:
-
+    
     def __init__(self, dynamic_range, compare_time=0.01, verbose=False):
+    
         self.dynamic_range = dynamic_range
         self.verbose = verbose
         self.compare_time = compare_time
 
-
+        
         self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+
         self.comp_gpio = 21
 
         GPIO.setmode(GPIO.BCM)
@@ -26,28 +27,47 @@ class R2R_ADC:
             GPIO.output(pin, bit)
 
     def sequential_counting_adc(self):
+       
         for code in range(256):
             self.number_to_dac(code)
             time.sleep(self.compare_time)
-
             if GPIO.input(self.comp_gpio) == 1:
                 if self.verbose:
                     print(f"Превышение достигнуто при коде {code}")
                 return code
-
-        if self.verbose:
-            print("Превышения не достигнуто, вернём 255")
         return 255
 
     def get_sc_voltage(self):
+        
         code = self.sequential_counting_adc()
         voltage = code * self.dynamic_range / 255.0
         if self.verbose:
-            print(f"Измеренный код: {code}, напряжение: {voltage:.3f} В")
+            print(f"[SC] Код: {code}, напряжение: {voltage:.3f} В")
+        return voltage
+
+    def successive_approximation_adc(self):
+        
+        code = 0
+        for bit in range(7, -1, -1):
+            test_code = code | (1 << bit)
+            self.number_to_dac(test_code)
+            time.sleep(self.compare_time)
+
+            if GPIO.input(self.comp_gpio) == 0:
+                code = test_code
+
+        if self.verbose:
+            print(f"[SAR] Найден код: {code}")
+        return code
+
+    def get_sar_voltage(self):
+        code = self.successive_approximation_adc()
+        voltage = code * self.dynamic_range / 255.0
+        if self.verbose:
+            print(f"[SAR] Код: {code}, напряжение: {voltage:.3f} В")
         return voltage
 
     def cleanup(self):
-
         self.number_to_dac(0)
         GPIO.cleanup(self.bits_gpio + [self.comp_gpio])
         if self.verbose:
@@ -62,16 +82,16 @@ if __name__ == "__main__":
 
     try:
         adc = R2R_ADC(dynamic_range=DYNAMIC_RANGE, compare_time=0.01, verbose=False)
-
-        print("Начинаем измерение напряжения. Для выхода нажмите Ctrl+C\n")
+        print("Начинаем измерение напряжения методом последовательного приближения (SAR).")
+        print("Для выхода нажмите Ctrl+C\n")
 
         while True:
-            voltage = adc.get_sc_voltage()
-            print(f"Напряжение: {voltage:.3f} В")
-            time.sleep(0.05)
+            voltage = adc.get_sar_voltage()
+            print(f"Напряжение (SAR): {voltage:.3f} В")
+            time.sleep(0.2)
 
     except KeyboardInterrupt:
-        print("\nПрерывание по запросу пользователя")
+        print("\nПрерывание пользователем")
 
     finally:
         if 'adc' in locals():
