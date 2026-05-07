6f0b7ef261cad183096049062216334bc271caa0
roggg63 <nik.rogov.07@mail.ru>
2026-05-07 18:06:29 +0300

    adc

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 def plot_voltage_vs_time(time, voltage, max_voltage):
     plt.figure(figsize=(10.6))
     plt.plot(time, voltage)
-    plt.grid
+    plt.grid()
     plt.ylim(0, max_voltage)
-    plt.show
+    plt.show()
 
 
-def plot_sampling_period_hist(time)
+def plot_sampling_period_hist(time):
     diff_time = []
     for i in range(len(time) - 1):
         diff_time.append(time[i + 1] - time[i])
 
     plt.figure(figsize = (10.6))
     plt.hist(diff_time)
-    plt.xlim(0, 00.6)
+    plt.xlim(0, 0.6)
     plt.xlabel("Период измерений, с")
     plt.ylabel("Количество измерений")
     plt.grid()
